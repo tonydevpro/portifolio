d9f26819797854adb873d70d4f0db19f97dd0023
tonydevpro <antonio.1358@hotmail.com>
2024-10-04 15:39:45 -0300

    desktop

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,45 @@
-var infProjeto = document.getElementsByClassName('infProjeto');
+var infProjeto = document.querySelector('.infProjeto');
 var abrir = document.querySelectorAll("[class^='pro-']");
 
     for (let i = 0; i < abrir.length; i++){
         abrir[i].addEventListener('click', ()=>{
-            infProjeto[0].style.display = 'block';
+            infProjeto.style.display = 'block';
         })
     }
-var fechar = document.getElementById('fechar').addEventListener('click', ()=>{
-    infProjeto[0].style.display = 'none';
+
+
+
+document.querySelectorAll("[class^='pro-']").forEach((item) => {
+    item.addEventListener('click', function() {
+        const info = this.getAttribute('data-info');
+        document.querySelector('.infProjeto').innerHTML = info;
+        document.querySelector('.infProjeto').style.display = 'block';
+        var fechar = document.getElementById('fechar').addEventListener('click', ()=>{
+            infProjeto.style.display = 'none';
+    });
+});
+
+
+
+
 })
 
+document.querySelectorAll("[class^='des-']").forEach((item) => {
+    item.addEventListener('click', function() {
+        const info = this.getAttribute('data-info');
+        document.querySelector('.infProjeto').innerHTML = info;
+        document.querySelector('.infProjeto').style.display = 'block';
+        var fechar = document.getElementById('fechar').addEventListener('click', ()=>{
+            infProjeto.style.display = 'none';
+    });
+});
+});
+
+document.getElementById("menu").addEventListener("click", function() {
+    let sidebar = document.getElementById("sidebar");
+    if (sidebar.style.left === "0px") {
+        sidebar.style.left = "-25%";
+    } else {
+        sidebar.style.left = "0px";
+    }
+});
